08f89dbf03465273b6d4602012f13f7c0b515c95
ManuelRamir <i620655@cibertec.edu.pe>
2025-09-30 03:20:03 +0000

    clase 29 de setiembre

---
+++ 16_app.py
@@ -0,0 +1,13 @@
+# Estructura de bucle For
+
+# Sintaxis
+# for value in sequence:
+#   instrucción, que es lo que debe hacer mientras se cumple el bucle
+
+# Ejemplo:
+
+# Listado de frutas
+paises = ["Brasil", "China", "Perú", "Colombia", "Chile"]
+
+for pais in paises:
+    print(pais)
